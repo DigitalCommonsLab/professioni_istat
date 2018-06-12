3c02088266a7c303e46071459b9075f9708237eb
Stefano Cudini <stefano.cudini@gmail.com>
2018-06-12 18:12:21 +0200

    url

--- src/main.js
+++ src/main.js
@@ -14,21 +14,30 @@ var bt = require('bootstrap');
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 
 var utils = require('./utils');
-
 var tree = require('./tree');
+
+var baseUrl = 'http://api-test.smartcommunitylab.it/t/sco.cartella/isfol/1.0.0';
+
+
+var tmpls = {
+  urlData: H.compile(baseUrl+'/istatLevel{{level}}/{{id}}')
+};
 
 $(function() {
 
   tree.init('#tree', {
     
-    urlData: 'data/example.json',
+    //urlData: 'data/example.json',
+    urlTmpl: baseUrl+'/istatLevel{level}/{id}',
 
-    onSelect: function(id) {
+    onSelect: function(id, level) {
 
-      console.log('tree onSelect', id)
+      console.log('tree onSelect', id);
+
+
 
 //TODO using utils.getData(url+id, function(json) { ... });
-      tree.update(id, json);
+      //tree.update(id, json);
 
     }
   })
